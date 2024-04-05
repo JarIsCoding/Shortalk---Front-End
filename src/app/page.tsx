18e6d4189d7bd51d9b33b02c15a2708002ff6ad3
JarIsCoding <derpyderp1098@gmail.com>
2024-04-05 15:11:33 -0700

    small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,44 +36,47 @@ export default function Home() {
   }
 
   return (
-    <div className='grid grid-flow-row justify-center'>
+    <div className='bg-lblue vh'>
+      <div className='grid grid-flow-row justify-center'>
 
-      <div className='py-24 text-center'>
-        <p className='text-[48px]'>
-          SHORTALK
-        </p>
-      </div>
+        <div className='py-24 text-center'>
+          <p className='text-[48px]'>
+            SHORTALK
+          </p>
+        </div>
 
-      <div className='bigCardBg rounded-md flex justify-center'>
-        <form className="flex max-w-md flex-col gap-4">
+        <div className='bigCardBg rounded-md flex justify-center'>
+          <form className="flex max-w-md flex-col gap-4">
 
-          {/* Top Text */}
-          <div className='text-center py-7 text-[32px]'>
-            Login
-          </div>
+            {/* Top Text */}
+            <div className='text-center py-7 text-[32px]'>
+              Login
+            </div>
 
-          {/* Username and password Input Field */}
-          <TextInput id="username" type="text" placeholder='Username' className='inputSize rounded-none' onChange={(e) => setUsername(e.target.value)} required />
-          <TextInput id="password1" type="password" placeholder='Password' className='inputSize rounded-none' onChange={(e) => setPassword(e.target.value)} required />
+            {/* Username and password Input Field */}
+            <input id="username" type="text" placeholder='Username' className='inputSize rounded-none' onChange={(e) => setUsername(e.target.value)} required />
+            <input id="password1" type="password" placeholder='Password' className='inputSize rounded-none' onChange={(e) => setPassword(e.target.value)} required />
 
-          <div className='py-3'>
-            <p onClick={() => router.push('/pages/signUpPage')} className='text-center pb-2'>
-              New to WWC? Create an profile!
-            </p>
-
-            <p onClick={() => { router.push('/pages/homePage'); setUsername('Guest') }} className='text-center'>
-              Or sign in as a guest
-            </p>
-          </div>
-
-          <div className='flex justify-center'>
-            <Button onClick={handleSubmit} className='loginBtn'>
-              <p className='text-[36px] text-center'>
-                Login
+            <div className='py-3'>
+              <p onClick={() => router.push('/pages/signUpPage')} className='text-center pb-2'>
+                New to WWC? <span className='underline'>Create an profile!</span>
               </p>
-            </Button>
-          </div>
-        </form>
+
+              <p onClick={() => { router.push('/pages/homePage'); setUsername('Guest') }} className='text-center'>
+                or <span className='underline'>Sign in as guest</span>
+              </p>
+            </div>
+
+            <div className='flex justify-center'>
+              <Button onClick={handleSubmit} className='loginBtn'>
+                <p className='text-[36px] text-center'>
+                  Login
+                </p>
+              </Button>
+            </div>
+
+          </form>
+        </div>
       </div>
     </div>
   );
