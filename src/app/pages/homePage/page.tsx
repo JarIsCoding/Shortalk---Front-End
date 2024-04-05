629e7fd496d31913f744e8971e57e4487db5a57f
TerryKrabs <thegoldenlemon1@gmail.com>
2024-04-05 15:19:35 -0700

    tgif

--- src/app/pages/homePage/page.tsx
+++ src/app/pages/homePage/page.tsx
@@ -1,14 +1,45 @@
 'use client'
-
+import NavBar from '@/app/components/NavBar'
 import React from 'react'
+import { Button } from "flowbite-react";
+
+
 
 const homePage = () => {
   return (
-    <div>
+
+
+    <div className='bg-lblue'>
       {/* Check if user is guest or signed in */}
-      Home Page
+      
+    <NavBar title='Welcome LEMONSQUIRT23'/>
+
+      <h1 className=''>SHORTALK</h1>
+
+
+      <Button size="" className='w-[450px] h-[150px] justify-center'>
+
+        <p className=''>Create A Room</p>
+
+      </Button>
+      <Button size="xl" className='w-[450px] h-[150px] justify-center'>
+
+        <p>Join A Room</p>
+
+      </Button>
+
+      <Button size="xl" className='w-[450px] h-[150px] justify-items-center'>
+
+        <p>Pass N' Play</p>
+
+      </Button>
+
+
+
     </div>
   )
 }
+
+
 
 export default homePage
