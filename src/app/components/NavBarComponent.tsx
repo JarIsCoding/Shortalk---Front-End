849705857e64d666a3974f0e83f30e88528707e3
JosephPoncini <jxponcini@gmail.com>
2024-04-05 11:54:59 -0700

    context

--- src/app/components/NavBarComponent.tsx
+++ src/app/components/NavBarComponent.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const NavBarComponent = () => {
   return (
-    <div className=''>NavBarComponent</div>
+    <div>NavBarComponent</div>
   )
 }
 
---
+++ src/context/Context.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { createContext, useContext, useState } from "react"
+
+// Creating Context
+// Wrapping our app
+// export our Custom Hook
+
+//First lets define our context
+
+interface IContextValue {
+    // tip: number | undefined
+    // setTip: (tip: number | undefined) => void
+    // bill: number
+    // setBill: (bill: number) => void
+    // groupSize: number
+    // setGroupSize: (groupSize: number) => void
+    // isCustom: boolean
+    // setIsCustom: (isCustom: boolean) => void
+}
+
+// {} as IContextValue is just giving placeholder values
+export const Context = createContext<IContextValue>({} as IContextValue);
+
+// functional component to pass the provided context to our children
+export const AppWrapper = ({ children, }: Readonly<{children: React.ReactNode;}>) => {
+    // const [tip, setTip] = useState<number | undefined>(undefined);
+    // const [bill, setBill] = useState<number>(0);
+    // const [groupSize, setGroupSize] = useState<number>(0);
+    // const [isCustom, setIsCustom] = useState<boolean>(false);
+
+    return(
+        <Context.Provider value={{}}>
+            {children}
+        </Context.Provider>
+    )
+}
+
+
+export const useAppContext = () => {
+    return useContext(Context);
+}
