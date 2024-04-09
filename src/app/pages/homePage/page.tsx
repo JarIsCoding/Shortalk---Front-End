037ab4c2430f5448214c222dbb0881133ccb9b28
TerryKrabs <thegoldenlemon1@gmail.com>
2024-04-09 11:07:05 -0700

    MOndayyyy

--- src/app/pages/homePage/page.tsx
+++ src/app/pages/homePage/page.tsx
@@ -7,25 +7,32 @@ import { Button } from "flowbite-react";
 const homePage = () => {
   return (
 
-    <div className='bg-lblue min-h-screen w-full Bg'>
-      {/* Check if user is guest or signed in */}
+    <div className='bg-lblue min-h-screen w-full Bg relative'>
+    {/* Check if user is guest or signed in */}
+    {/* Title */}
+    <div className='relative'>
+      <NavBar title='Welcome LEMONSQUIRT23'/>   
+      <div className="absolute top-0 right-0 mr-4 mt-4">
+        {/* Add your images here */}
+        <img src="path_to_your_image" alt="Image 1" className="w-10 h-10 mr-2" />
+        <img src="path_to_your_image" alt="Image 2" className="w-10 h-10" />
+      </div>     
+    </div>
 
-      {/* Title */}
-    <NavBar title='Welcome LEMONSQUIRT23'/>
 
     <div className='mt-4'>
       {/* Tilted SHORTALK */}
       <h1 className='font-LuckiestGuy text-dblue px-10 h-[100px] text-5xl -rotate-12 pl-96 shortOut'>SHORTALK</h1>
     {/* Buttons */}
-      <Button size="" className='w-[450px] h-[150px] mx-auto my-4 bg-dblue'>
+      <Button size="" className='w-[450px] h-[130px] mx-auto my-4 bg-dblue'>
         <p className='font-LuckiestGuy text-white px-10 h-[100px] text-4xl flex items-center'>Create A Room</p>
       </Button>
 
-      <Button size="xl" className='w-[450px] h-[150px] justify-center mx-auto my-4 bg-dblue'>
+      <Button size="xl" className='w-[450px] h-[130px] justify-center mx-auto my-5 bg-dblue'>
         <p className='font-LuckiestGuy text-white px-10 h-[100px] text-4xl flex items-center'>Join A Room</p>
       </Button>
 
-      <Button size="xl" className='w-[450px] h-[150px] justify-items-center mx-auto my-4 bg-dblue'>
+      <Button size="xl" className='w-[450px] h-[130px] justify-items-center mx-auto my-4 bg-dblue'>
         <p className='font-LuckiestGuy text-white px-10 h-[100px] text-4xl flex items-center'>Pass N' Play</p>
       </Button>
       </div>
