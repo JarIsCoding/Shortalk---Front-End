f99edf5803b1a436755f2421289c4d47026731f7
JarIsCoding <derpyderp1098@gmail.com>
2024-04-10 13:46:00 -0700

    friends tab changes

--- src/app/pages/homePage/page.tsx
+++ src/app/pages/homePage/page.tsx
@@ -10,7 +10,7 @@ import FriendsTab from '@/app/components/FriendsTab';
 
 const HomePage = () => {
 
-  const [isFriendsOn, setIsFriendsOn] = useState<boolean>(true);
+  const [isFriendsOn, setIsFriendsOn] = useState<boolean>(false);
 
   const handleClick = () => {
     setIsFriendsOn(!isFriendsOn);
@@ -18,48 +18,48 @@ const HomePage = () => {
 
   return (
 
-<div className='bg-lblue min-h-screen w-full Bg relative'>
+    <div className='bg-lblue min-h-screen w-full Bg relative'>
 
-  {
-    isFriendsOn && <FriendsTab/>
-  }
-  
-  {/* Check if user is guest or signed in */}
-  {/* Title */}
-  <div className='relative'>
-    <NavBar title='Welcome LEMONSQUIRT23'/>   
-    \
-    <div className="absolute top-6 right-0 mr-10 flex">
-      {/*NavBar Icons/Buttons*/}
-      <Button onClick={() => handleClick} className="bg-clear">
-        <Image src={RulesPic} alt="RulesPicture" className="w-35px h-30px rulesNav"/>
-      </Button>
-      <Button onClick={() => handleClick} className='bg-clear '>
-        <Image src={FriendsPic} alt="FriendsPicture" className="w-35px h-30px friendsNav"/>
-      </Button>
-    </div>     
-  </div>
-
-    <div className='mt-4'>
-      {/* Tilted SHORTALK */}
-      <div className='pulse'>
-      <h1 className='font-LuckiestGuy text-dblue px-10 h-[100px] text-5xl -rotate-12 pl-96 shortOut'>SHORTALK</h1>
+      <div className={`${isFriendsOn ? 'block' : 'hidden'}`}>
+        <FriendsTab />
       </div>
-    {/* Buttons */}
-      <Button size="" className='w-[450px] h-[130px] mx-auto my-4 bg-dblue'>
-        <p className='font-LuckiestGuy text-white px-10 h-[100px] text-4xl flex items-center'>Create A Room</p>
-      </Button>
 
-      <Button size="xl" className='w-[450px] h-[130px] justify-center mx-auto my-5 bg-dblue'>
-        <p className='font-LuckiestGuy text-white px-10 h-[100px] text-4xl flex items-center'>Join A Room</p>
-      </Button>
+      {/* Check if user is guest or signed in */}
+      {/* Title */}
+      <div className='relative'>
+        <NavBar title='Welcome LEMONSQUIRT23' />
+        \
+        <div className="absolute top-6 right-0 mr-10 flex">
+          {/*NavBar Icons/Buttons*/}
+          <Button className="bg-clear">
+            <Image src={RulesPic} alt="RulesPicture" className="w-35px h-30px rulesNav" />
+          </Button>
+          <Button onClick={() => {isFriendsOn ? setIsFriendsOn(false) : setIsFriendsOn(true)}} className={`bg-clear`}>
+            <Image src={FriendsPic} alt="FriendsPicture" className="w-35px h-30px friendsNav" />
+          </Button>
+        </div>
+      </div>
 
-      <Button size="xl" className='w-[450px] h-[130px] justify-items-center mx-auto my-4 bg-dblue'>
-      <p className='font-LuckiestGuy text-white px-10 h-[100px] text-4xl flex items-center'>Pass N{"'"} Play</p>
-      </Button>
+      <div className='mt-4'>
+        {/* Tilted SHORTALK */}
+        <div className='pulse'>
+          <h1 className='font-LuckiestGuy text-dblue px-10 h-[100px] text-5xl -rotate-12 pl-96 shortOut w-[90%]'>SHORTALK</h1>
+        </div>
+        {/* Buttons */}
+        <Button size="" className='w-[450px] h-[130px] mx-auto my-4 bg-dblue'>
+          <p className='font-LuckiestGuy text-white px-10 h-[100px] text-4xl flex items-center'>Create A Room</p>
+        </Button>
+
+        <Button size="xl" className='w-[450px] h-[130px] justify-center mx-auto my-5 bg-dblue'>
+          <p className='font-LuckiestGuy text-white px-10 h-[100px] text-4xl flex items-center'>Join A Room</p>
+        </Button>
+
+        <Button size="xl" className='w-[450px] h-[130px] justify-items-center mx-auto my-4 bg-dblue'>
+          <p className='font-LuckiestGuy text-white px-10 h-[100px] text-4xl flex items-center'>Pass N{"'"} Play</p>
+        </Button>
       </div>
     </div>
-    )
-  }
+  )
+}
 
 export default HomePage
