d9dea43697fff1ceb26bf036b82762fa43fb15dd
JosephPoncini <jxponcini@gmail.com>
2024-04-10 22:52:46 -0700

    Home page fixes

---
+++ src/app/pages/passAndPlayLobby/page.tsx
@@ -0,0 +1,12 @@
+import NavBar from '@/app/components/NavBar'
+import React from 'react'
+
+const PassAndPlayLobby = () => {
+  return (
+    <div>
+        <NavBar title="Pass N' Play Settings"/>
+    </div>
+  )
+}
+
+export default PassAndPlayLobby
