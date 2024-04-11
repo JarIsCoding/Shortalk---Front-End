51f96bcb33f40a1528d0ef86e74f31a37fbea861
JosephPoncini <jxponcini@gmail.com>
2024-04-11 12:34:58 -0700

    PNP Lobby Updated

---
+++ src/app/components/TeamListPNP.tsx
@@ -0,0 +1,18 @@
+'use client'
+import React from 'react'
+
+const TeamListPNP = () => {
+  return (
+    <div className=' space-y-5 w-[350px]'>
+        <div className='flex flex-row justify-between whitespace-nowrap items-center'>
+            <div className=' font-LuckiestGuy text-dblue text-3xl mr-5'>Team Name</div>
+            <input className=' w-full h-6' type="text" name="" id="" />
+        </div>
+        <div className=' bg-white rounded-lg w-full h-[275px] p-[10px]'>
+
+        </div>
+    </div>
+  )
+}
+
+export default TeamListPNP
--- src/app/pages/passAndPlayLobby/page.tsx
+++ src/app/pages/passAndPlayLobby/page.tsx
@@ -1,10 +1,17 @@
 import NavBar from '@/app/components/NavBar'
+import TeamListPNP from '@/app/components/TeamListPNP'
 import React from 'react'
 
 const PassAndPlayLobby = () => {
   return (
     <div>
         <NavBar title="Pass N' Play Settings"/>
+        <div className='flex flex-col items-center space-y-10 pt-20'>
+          <div className='flex flex-row justify-between w-[850px]'>
+              <TeamListPNP/>
+              <TeamListPNP/>
+          </div>
+        </div>
     </div>
   )
 }
