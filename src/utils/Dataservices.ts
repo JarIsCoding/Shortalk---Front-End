618c07443f9247653cc8f8a030aad45051fab1e5
JosephPoncini <jxponcini@gmail.com>
2024-04-12 13:49:56 -0700

    randomized cards

--- src/utils/Dataservices.ts
+++ src/utils/Dataservices.ts
@@ -1,6 +1,6 @@
 import { ICard, ICardData, IToken, IUserData, IUserInfo } from "@/Interfaces/Interfaces"
 import { Context } from "@/context/Context"
-
+import * as wordData from '../words.json';
 
 const url = "https://shortalkapi.azurewebsites.net"
 
@@ -68,16 +68,40 @@ export const checkToken = () => {
     return result
 }
 
-export const getCard = async () => {
-    const promise = await fetch('../words.json');
-    const data:Promise<ICardData[]> = promise.json();
+// export const getCardData = async () => {
+//     const promise = await fetch('../words.json');
+//     const data: Promise<ICardData[]> = promise.json();
 
+//     return data
+// }
 
-    let randNum = Math.random()*(await data).length;
+export const getCard = () => {
+    let data:ICardData[] = wordData;
+
+    let randNum: number = Math.floor(Math.random() * data.length);
+    let topWord = data[randNum].Top
+    let botWords = data[randNum].Bottom;
+    let botWord = botWords[Math.floor(Math.random() * botWords.length)]
 
     let coinflip = Math.round(Math.random());
 
-    // let card: ICard = 
+    let card: ICard = {} as ICard
+    switch (coinflip) {
+        case 0:
+            card = {
+                top: topWord,
+                bottom: `${topWord} ${botWord}`
+            }
+            break;
+        case 1:
+            card = {
+                top: botWord,
+                bottom: `${topWord} ${botWord}`
+            }
+            break;
+        default:
+            break;
+    }
 
-    return data
+    return card
 }
