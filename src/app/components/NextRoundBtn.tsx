95cb7965bc5a459da9224fd295c8bbbaa13bf63b
JarIsCoding <derpyderp1098@gmail.com>
2024-04-23 15:50:13 -0700

    winpage design created with new btn

---
+++ src/app/components/NextRoundBtn.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import React from 'react'
+import Image from 'next/image';
+import BlueArrow from '@/app/assets/BlueArrow.png'
+
+
+const NextRoundBtn = () => {
+  return (
+    <div className=' rounded-2xl border-2 border-black w-[325px] h-[75px] bg-[#B7E9F4] flex justify-between items-center px-4'>
+        <div className=' font-LuckiestGuy text-[40px] text-dblue tracking-wide'>Next Round</div>
+        <Image src={BlueArrow} alt='blue arrow' width={50} height={40}/>
+    </div>
+  )
+}
+
+export default NextRoundBtn
