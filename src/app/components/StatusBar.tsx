3aeedfe258a36a753c07261b1bbde3a05cf418d1
JarIsCoding <derpyderp1098@gmail.com>
2024-05-17 08:31:53 -0700

    responsivenss stuff

--- src/app/components/StatusBar.tsx
+++ src/app/components/StatusBar.tsx
@@ -7,7 +7,7 @@ import Timer from './Timer'
 const StatusBar = (props: IStatusBar) => {
   console.log(props)
   return (
-    <div className=' bg-status rounded-[20px] px-[100px] py-[10px] font-Roboto text-textGray w-full h-[75px] flex justify-between items-center text-2xl cursor-default'>
+    <div className=' bg-status rounded-[20px] lg:px-[100px] px-5 py-[10px] font-Roboto text-textGray w-full h-[75px] flex justify-between items-center text-2xl cursor-default'>
         {props.time && <Timer initialTime={props.time}/>}
         {props.teamName && <div>{"Team: " + props.teamName}</div>}
         {props.user && <div>{"Player: " + props.user}</div>}        
